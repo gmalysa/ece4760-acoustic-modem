4d8be6e45483cd7afda72eec273ce67e38d94cb1
4romanen <4romanen@27756a54-6646-6b34-56b7-074c8a0e1335>
2010-04-17 03:26:41 +0000

    New, much simpler circular input buffer

--- final_for_real.c
+++ final_for_real.c
@@ -30,6 +30,7 @@
 
 // Decoding defines
 #define IN_BUFFER_SIZE 64
+#define NOTIFY_FREQ 16
 
 // Send variables
 uint8_t output_sample_num = 0;
@@ -41,17 +42,15 @@ uint8_t bitmasks[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b0001000
 uint8_t freqCache[64];
 
 // Receive variables
-volatile int8_t input_buffer[2][IN_BUFFER_SIZE];
-volatile uint8_t input_buffer_num = 0;
+volatile int8_t input_buffer[IN_BUFFER_SIZE];
 volatile uint8_t input_ready = 0;
 int8_t resample_buffer_sin[7];
 int8_t resample_buffer_cos[7];
-uint8_t resample_buffer_sin_position = 0;
-uint8_t resample_buffer_cos_position = 0;
+uint8_t resample_buffer_position = 0;
 int16_t sin_acc = 0, cos_acc = 0;
 uint16_t input_buffer_resample_position = 0;
-uint16_t last_input_buffer_resample_position = 0;
 uint8_t input_buffer_pos = 0;
+volatile uint8_t public_input_buffer_position = 0;
 void init();
 
 ISR(TIMER1_COMPA_vect) {
@@ -60,13 +59,15 @@ ISR(TIMER1_COMPA_vect) {
 	PORTC = freqCache[output_sample_num];
 	output_sample_num = (output_sample_num + 1) & 0x3f;
 	PORTD |= _BV(PB7);	
-	input_buffer[input_buffer_num][input_buffer_pos] = ADCH - 128;
+	input_buffer[input_buffer_pos] = ADCH - 128;
 	//UDR0 = ADCH;
 	ADCSRA |= _BV(ADSC);
+	public_input_buffer_position = input_buffer_pos;
+	if((input_buffer_pos & 0x1f) == 16) {
+		input_ready = 1;
+	}
 	input_buffer_pos = (input_buffer_pos+1) & (IN_BUFFER_SIZE - 1);
 	if(output_sample_num == 0) {
-		input_buffer_num ^= 1;
-		input_ready = 1;
 		output_bitpattern = 0;
 		for(i=0; i<8; ++i) {
 			if(output_buffer_status[i] == IDLE) {continue;}
@@ -94,12 +95,10 @@ ISR(TIMER1_COMPB_vect, ISR_NAKED) {
 }
 
 int main() {
-	int8_t this_input_buffer = -1;
-	int8_t input_sin_sample, input_cos_sample;
-	uint8_t analyze = 1;
-	uint16_t analyze_output, last_analyze_output = 0; 
+	int8_t input_sample;
+	uint16_t analyze_output;
 	int16_t analyze_temp;
-	uint8_t start_found = 0;
+	uint8_t start = 0;
 	init();
 	while(1) {
 		if(UCSR0A & _BV(RXC0)) {	// We've received a serial byte
@@ -110,68 +109,44 @@ int main() {
 			//next_buffer = (next_buffer + 1) & 0x7;
 		}
 		if(input_ready) {
-			if(this_input_buffer < 0) {
-				this_input_buffer = input_buffer_num ^ 1;
-			}
-			if(resample_buffer_sin_position != resample_buffer_cos_position) { // Sin and cos split buffers, so we need to do cos twice
-				cos_acc -= resample_buffer_cos[resample_buffer_cos_position];
-				input_cos_sample = input_buffer[this_input_buffer][fix2int(input_buffer_resample_position - float2fix((10./4.)*3.))];
-				cos_acc += input_cos_sample;
-				resample_buffer_cos[resample_buffer_cos_position++] = input_cos_sample;
-				if(resample_buffer_cos_position > 6) {
-					resample_buffer_cos_position = 0;
+			if(fix2int(input_buffer_resample_position + float2fix(10./4.)) < public_input_buffer_position || fix2int(input_buffer_resample_position + float2fix(10./4.)) - public_input_buffer_position > IN_BUFFER_SIZE / 2) {
+				sin_acc -= resample_buffer_sin[resample_buffer_position];
+				input_sample = input_buffer[fix2int(input_buffer_resample_position)];
+				sin_acc += input_sample;
+				resample_buffer_sin[resample_buffer_position] = input_sample;
+
+				cos_acc -= resample_buffer_cos[resample_buffer_position];
+				input_sample = input_buffer[fix2int(input_buffer_resample_position + float2fix(10./4.))];
+				cos_acc += input_sample;
+				resample_buffer_cos[resample_buffer_position] = input_sample;
+
+				if(++resample_buffer_position > 6) {
+					resample_buffer_position = 0;
+					//input_buffer_resample_position += float2fix(4.);
+					if(start--) {
+						input_buffer_resample_position -= int2fix(6);
+					}
+				}
+				input_buffer_resample_position += float2fix(10.);
+				analyze_temp = sin_acc >> 2;
+				analyze_output = analyze_temp * analyze_temp;
+				analyze_temp = cos_acc >> 2;
+				analyze_output += (analyze_temp * analyze_temp);
+				if(analyze_output > 50) {
+					UDR0 = '-';
+					sin_acc = cos_acc = 0;
+					memset(resample_buffer_sin, 0, sizeof(uint8_t) * 7);
+					memset(resample_buffer_cos, 0, sizeof(uint8_t) * 7);
+					if(start==0) {
+						start = 8;
+						resample_buffer_position = 0;
+						input_buffer_resample_position -= int2fix(6);
+					}
 				}
 			}
-			// Update sin values
-			sin_acc -= resample_buffer_sin[resample_buffer_sin_position];
-			input_sin_sample = input_buffer[this_input_buffer][fix2int(input_buffer_resample_position)];
-			sin_acc += input_sin_sample;
-			resample_buffer_sin[resample_buffer_sin_position++] = input_sin_sample;
-			if(resample_buffer_sin_position > 6) {
-				resample_buffer_sin_position = 0;
-			}
-			// Update cos values iff the sample is not in the next buffer
-			if(input_buffer_resample_position + float2fix(10./4.) > input_buffer_resample_position) {
-				cos_acc -= resample_buffer_cos[resample_buffer_cos_position];
-				input_cos_sample = input_buffer[this_input_buffer][fix2int(input_buffer_resample_position + float2fix(10./4.))];
-				cos_acc += input_cos_sample;
-				resample_buffer_cos[resample_buffer_cos_position++] = input_cos_sample;
-				if(resample_buffer_cos_position > 6) {
-					resample_buffer_cos_position  = 0;
-				}
-				analyze = 1;
-			}
-			// Cos sample is in the next buffer, delay processing
-			else {
-				analyze = 0;
-			}
-			if(resample_buffer_sin_position == 0 && start_found--) {
-				input_buffer_resample_position += int2fix(4);
-			}
-			last_input_buffer_resample_position = input_buffer_resample_position;
-			input_buffer_resample_position += float2fix(10.);
-			if(input_buffer_resample_position < last_input_buffer_resample_position) { // We finished this buffer
+			else  {
 				input_ready = 0;
-				this_input_buffer = -1;
 			}
-		}
-		if(analyze) {
-			analyze_temp = sin_acc >> 2;
-			analyze_output = analyze_temp * analyze_temp;
-			analyze_temp = cos_acc >> 2;
-			analyze_output += (analyze_temp * analyze_temp);
-			if(analyze_output > 100){// && analyze_output < last_analyze_output) {
-				UDR0 = '-';
-				sin_acc = cos_acc = 0;
-				memset(resample_buffer_sin, 0, sizeof(uint8_t) * 7);
-				memset(resample_buffer_cos, 0, sizeof(uint8_t) * 7);
-				if(start_found == 0) {
-					start_found = 8;
-					resample_buffer_sin_position = resample_buffer_cos_position = 6;
-				}
-				//input_buffer_resample_position += float2fix(10.);
-			}
-			last_analyze_output = analyze_output;
 		}
 	}
 }
