e93fc3759197885ab91e77b8174182f85a706266
4romanen <4romanen@27756a54-6646-6b34-56b7-074c8a0e1335>
2010-04-19 18:01:14 +0000

    Greg's matlab-type code with minor changes

--- trunk/final_for_real.c
+++ trunk/final_for_real.c
@@ -41,6 +41,9 @@ uint8_t output_bitpattern = 0;
 uint8_t bitmasks[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
 uint8_t freqCache[64];
 
+const uint16_t f4000_increment = float2fix(10.);
+const uint16_t f4000_quarterphase = float2fix(2.5);
+
 // Receive variables
 volatile int8_t input_buffer[IN_BUFFER_SIZE];
 volatile uint8_t input_ready = 0;
@@ -63,7 +66,7 @@ ISR(TIMER1_COMPA_vect) {
 	//UDR0 = ADCH;
 	ADCSRA |= _BV(ADSC);
 	public_input_buffer_position = input_buffer_pos;
-	input_ready |= ((input_buffer_pos & (NOTIFY_FREQ * 2 - 1)) == NOTIFY_FREQ);
+	//input_ready |= ((input_buffer_pos & (NOTIFY_FREQ * 2 - 1)) == NOTIFY_FREQ);
 	input_buffer_pos = (input_buffer_pos+1) & (IN_BUFFER_SIZE - 1);
 	if(output_sample_num == 0) {
 		output_bitpattern = 0;
@@ -106,8 +109,18 @@ int main() {
 			UDR0 = output_buffer[next_buffer];	
 			//next_buffer = (next_buffer + 1) & 0x7;
 		}
-		if(input_ready) {
-			if(fix2int(input_buffer_resample_position + float2fix(10./4.)) < public_input_buffer_position){// || fix2int(input_buffer_resample_position + float2fix(10./4.)) - public_input_buffer_position > IN_BUFFER_SIZE / 2) {
+		//if(input_ready) {
+			if(fix2int(input_buffer_resample_position + f4000_quarterphase) < public_input_buffer_position){// || fix2int(input_buffer_resample_position + float2fix(10./4.)) - public_input_buffer_position > IN_BUFFER_SIZE / 2) {
+				UDR0 = public_input_buffer_position;
+				sin_acc -= resample_buffer_sin[resample_buffer_position];
+				input_sample = input_buffer[fix2int(input_buffer_resample_position)];
+				sin_acc += input_sample;
+				resample_buffer_sin[resample_buffer_position] = input_sample;
+
+				cos_acc -= resample_buffer_cos[resample_buffer_position];
+				input_sample = input_buffer[fix2int(input_buffer_resample_position + f4000_quarterphase)];
+				cos_acc += input_sample;
+				resample_buffer_cos[resample_buffer_position] = input_sample;
 
 				analyze_temp = sin_acc >> 2;
 				analyze_output = analyze_temp * analyze_temp;
@@ -139,23 +152,14 @@ int main() {
 					}
 					detected = 0;
 				}
-				sin_acc -= resample_buffer_sin[resample_buffer_position];
-				input_sample = input_buffer[fix2int(input_buffer_resample_position)];
-				sin_acc += input_sample;
-				resample_buffer_sin[resample_buffer_position] = input_sample;
-
-				cos_acc -= resample_buffer_cos[resample_buffer_position];
-				input_sample = input_buffer[fix2int(input_buffer_resample_position + float2fix(10./4.))];
-				cos_acc += input_sample;
-				resample_buffer_cos[resample_buffer_position] = input_sample;
 
 				++resample_buffer_position;
-				input_buffer_resample_position += float2fix(10.);
+				input_buffer_resample_position += f4000_increment;
 			}
-			else  {
+			/*else  {
 				input_ready = 0;
 			}
-		}
+		}*/
 	}
 }
 
