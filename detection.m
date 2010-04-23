b7b95c1f0c191f53a07e70129174a246a4f7225e
foxinforestfire <foxinforestfire@27756a54-6646-6b34-56b7-074c8a0e1335>
2010-04-23 20:33:45 +0000

    works well, matlab integration

--- detection.m
+++ detection.m
@@ -8,8 +8,9 @@ hold on;
 fs = 40000;
 f = 4000;
 buflen = 7;
-offset = 27;
-thresh = 6000;
+offset = 26;
+thresh = 2000;
+derv_thresh = 6;
 
 % Sample input, with some amount of offset from the start of our buffer
 data = hann(64)' .* 40 .*cos(2*pi*(f/fs)*[0:63]);
@@ -18,7 +19,28 @@ input(offset:offset+63) = data;
 input(offset+64:offset+63+64) = data;
 input(offset+64+64+64:offset+63+64+64+64) = data;
 
-input = x;
+try
+    serial = serial('COM4', 'BaudRate', 230400, 'InputBufferSize', 1024, 'Terminator', '');
+catch exception
+    delete(serial);
+    clear serial;
+    serial = serial('COM4', 'BaudRate', 230400, 'InputBufferSize', 1024, 'Terminator', '');
+end
+try
+    fopen(serial);
+catch exception
+    delete(serial);
+    clear serial;
+    serial = serial('COM4', 'BaudRate', 230400, 'InputBufferSize', 1024, 'Terminator', '');
+    fopen(serial);
+end
+fprintf(serial, 'f');
+input = fread(serial, 1023, 'int8');
+fclose(serial);
+delete(serial);
+clear serial;
+
+%input = x;
 
 % Add noise for fun
 %input = input + 4*randn(1, 1024);
@@ -38,15 +60,19 @@ ssum = 0;
 start = 0;
 samples = 0;
 detected = 0;
+prev_csum = 0;
+prev_ssum = 0;
 while (k < length(input))
     % Find integer offsets
     cIndex = floor(j);
     sIndex = floor(k);
     
     % Update running structures
+    prev_csum = csum;
     csum = csum - rc(i);
     rc(i) = input(cIndex);
     csum = csum + rc(i);
+    prev_ssum = ssum;
     ssum = ssum - rs(i);
     rs(i) = input(sIndex);
     ssum = ssum + rs(i);
@@ -61,16 +87,26 @@ while (k < length(input))
     is(l) = k;
     
     % Attempt detection
+    trigger = 0;
     if (csum^2 + ssum^2) > thresh
-        if (0 == start)
+        if abs(csum) > abs(ssum) 
+            if (prev_csum - csum) < derv_thresh && (prev_csum - csum) > -derv_thresh
+                trigger = 1;
+            end
+        else
+            if (prev_ssum - ssum) < derv_thresh && (prev_ssum - ssum) > -derv_thresh
+                trigger = 1;
+            end
+        end
+        if (0 == start && trigger == 1)
 %             rc = zeros(1, buflen);
 %             rs = zeros(1, buflen);
 %             csum = 0;
 %             ssum = 0;
             samples = buflen;
             start = 9;
-            j = j + 10;
-            k = k + 10;
+%            j = j + 10;
+%            k = k + 10;
         end
     end
     
@@ -93,10 +129,8 @@ while (k < length(input))
         end
         
         if (detected && start > 0)
-            display('1');
             start = start - 1;
         elseif (start > 0)
-            display('0');
             start = start -1;
         end
         detected = 0;
