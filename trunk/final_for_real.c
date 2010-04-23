e4cb6ddc0645ffdd736c842218449c07878b18b3
foxinforestfire <foxinforestfire@27756a54-6646-6b34-56b7-074c8a0e1335>
2010-04-23 20:33:45 +0000

    works well, matlab integration

--- trunk/final_for_real.c
+++ trunk/final_for_real.c
@@ -31,7 +31,8 @@
 // Decoding defines
 #define IN_BUFFER_SIZE 64
 #define NOTIFY_FREQ 16
-#define THRESH 6000
+#define THRESH 2000
+#define DERIVATIVE_THRESH 6
 
 // Send variables
 uint8_t output_sample_num = 0;
@@ -43,10 +44,15 @@ uint8_t bitmasks[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b0001000
 uint8_t freqCache[64];
 
 // Debug variables
-#ifdef DEBUG
-	int8_t debugCache[640];
+//#define DEBUG_DUMP
+//#define DEBUG_THRESH
+#ifdef DEBUG_DUMP
+	int8_t debugCache[1024];
+	volatile uint16_t debugCache_p = 0;
 	uint16_t sample_dump_counter = 640;
 	volatile uint16_t dbg_counter = 0;
+	volatile uint8_t debug_enabled = 1;
+	uint16_t curr_debug_output;
 #endif
 
 const uint16_t f4000_increment = float2fix(10.);
@@ -58,7 +64,7 @@ volatile uint8_t input_ready = 0;
 int8_t resample_buffer_sin[7] = {0,0,0,0,0,0,0};
 int8_t resample_buffer_cos[7] = {0,0,0,0,0,0,0};
 uint8_t resample_buffer_position = 0;
-int16_t sin_acc = 0, cos_acc = 0;
+int16_t sin_acc = 0, cos_acc = 0, prev_sin_acc = 0, prev_cos_acc = 0, last_derv_sin = 0, last_derv_cos = 0;
 uint16_t input_buffer_resample_position = 0;
 uint8_t input_buffer_pos = 0;
 volatile uint8_t public_input_buffer_position = 0;
@@ -71,18 +77,22 @@ ISR(TIMER1_COMPA_vect) {
 	output_sample_num = (output_sample_num + 1) & 0x3f;
 	PORTD |= _BV(PB7);	
 	input_buffer[input_buffer_pos] = ADCH - 0x7F;
-	#ifdef DEBUG
-		if (input_buffer[input_buffer_pos] > 5 || input_buffer[input_buffer_pos] < -5 || sample_dump_counter < 640) {
+	ADCSRA |= _BV(ADSC);
+	public_input_buffer_position = input_buffer_pos;
+	#ifdef DEBUG_DUMP
+		if(debug_enabled) {
+			debugCache[debugCache_p] = input_buffer[input_buffer_pos];
+			debugCache_p = (debugCache_p + 1) & 0x3ff;
+		}
+		/*if (input_buffer[input_buffer_pos] > 5 || input_buffer[input_buffer_pos] < -5 || sample_dump_counter < 640) {
 			if (sample_dump_counter == 640) {
 				sample_dump_counter = 0;
 				dbg_counter = 640;
 			}
 			debugCache[sample_dump_counter] = input_buffer[input_buffer_pos];
 			sample_dump_counter++;
-		}
+		}*/
 	#endif
-	ADCSRA |= _BV(ADSC);
-	public_input_buffer_position = input_buffer_pos;
 	//input_ready |= ((input_buffer_pos & (NOTIFY_FREQ * 2 - 1)) == NOTIFY_FREQ);
 	input_buffer_pos = (input_buffer_pos+1) & (IN_BUFFER_SIZE - 1);
 	if(output_sample_num == 0) {
@@ -120,11 +130,11 @@ int main() {
 	int8_t input_sample;
 	int32_t analyze_output = 0;
 	uint8_t start = 0;
-	uint8_t x =0, i=0;
+	uint8_t x =0;
 	uint8_t output_char = 0;
 	init();
 	while(1) {
-		#ifdef DEBUG
+		#ifdef DEBUG_DUMP
 			while (dbg_counter) {
 				if (UCSR0A & _BV(UDRE0)) {
 					UDR0 = debugCache[640-dbg_counter];
@@ -143,6 +153,8 @@ int main() {
 		if((x <= public_input_buffer_position && public_input_buffer_position - x <= 10) ||
 		   (x > public_input_buffer_position && x - public_input_buffer_position > 10)) {
 			//UDR0 = public_input_buffer_position;
+			last_derv_sin = prev_sin_acc - sin_acc;
+			prev_sin_acc = sin_acc;
 			sin_acc -= resample_buffer_sin[resample_buffer_position];
 			input_sample = input_buffer[fix2int(input_buffer_resample_position)];
 			sin_acc += input_sample;
@@ -151,6 +163,8 @@ int main() {
 			//	UDR0 = input_sample;
 			//UDR0 = sin_acc;
 
+			last_derv_cos = prev_cos_acc - cos_acc;
+			prev_cos_acc = cos_acc;
 			cos_acc -= resample_buffer_cos[resample_buffer_position];
 			input_sample = input_buffer[fix2int(input_buffer_resample_position + f4000_quarterphase)];
 			cos_acc += input_sample;
@@ -166,33 +180,56 @@ int main() {
 			}	
 
 			if(analyze_output > THRESH && start == 0) {
-				sin_acc = cos_acc = 0;
-				/*for(i=0; i<7; ++i) {
-					resample_buffer_sin[i] = 0;
-					resample_buffer_cos[i] = 0;
-				}*/
-				start = 9;
-				output_char = 0;
-				resample_buffer_position = 6;
-				input_buffer_resample_position += int2fix(10);
+				if(abs(sin_acc) > abs(cos_acc)) {
+					if(((prev_sin_acc - sin_acc) ^ last_derv_sin) & 0x8000) {
+						start = 9;
+					}
+				}
+				else {
+					if(((prev_cos_acc - cos_acc) ^ last_derv_cos) & 0x8000) {
+						start = 9;
+					}
+				}
+				if(start == 9) {
+					sin_acc = cos_acc = 0;
+					/*for(i=0; i<7; ++i) {
+						resample_buffer_sin[i] = 0;
+						resample_buffer_cos[i] = 0;
+					}*/
+					output_char = 0;
+					resample_buffer_position = 6;
+					input_buffer_resample_position -= int2fix(10);
+				}
 			}
 
 			if(resample_buffer_position++ == 6) {
 				resample_buffer_position = 0;
 				//input_buffer_resample_position -= float2fix(4.);
 				if (start > 0) {
+					#ifdef DEBUG_THRESH
+						UDR0 =  analyze_output >> 8;
+					#endif
 					output_char = output_char << 1;
 					if(analyze_output > THRESH) {
 						output_char |= 1;
 					}
 					sin_acc = cos_acc = 0;
-					for(i=0; i<7; ++i) {
-						resample_buffer_sin[i] = 0;
-						resample_buffer_cos[i] = 0;
-					}
+					memset(resample_buffer_sin, 0, sizeof(int8_t) * 7);
+					memset(resample_buffer_cos, 0, sizeof(int8_t) * 7);
 					input_buffer_resample_position -= int2fix(6);
 					if(--start == 0) {
-						UDR0 = output_char;
+						#if !defined(DEBUG_DUMP) && !defined(DEBUG_THRESH)
+							UDR0 = output_char;
+						#elif defined(DEBUG_DUMP)
+							debug_enabled = 0;
+							curr_debug_output = debugCache_p+1;
+							while(curr_debug_output != debugCache_p) {
+								while(!(UCSR0A & _BV(UDRE0))) {;}
+								UDR0 = debugCache[curr_debug_output];
+								curr_debug_output = (curr_debug_output+1) & 0x3ff;
+							}
+							debug_enabled = 1;
+						#endif
 					}
 				}
 				/*if(detected && start > 0) {
@@ -231,9 +268,12 @@ void init() {
 	/* Setup UART */
 	UCSR0B = _BV(RXEN0) | _BV(TXEN0);	// Enable receive and transmit
 	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);		// Set 8-bit frames
-	//UCSR0A = _BV(U2X0);
-
-	UBRR0 = 129;	// Set baud rate to 2400
+	#if defined(DEBUG_DUMP) || defined(DEBUG_THRESH)
+		UCSR0A = _BV(U2X0);
+		UBRR0 = 10;		// Set baud rate to 230400
+	#else
+		UBRR0 = 520;	// Set baud rate to 9600
+	#endif
 
 	//memset(resample_buffer_sin, 0, sizeof(int8_t) * 7);
 	//memset(resample_buffer_cos, 0, sizeof(int8_t) * 7);
